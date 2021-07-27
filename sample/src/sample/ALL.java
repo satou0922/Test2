195c57021007addee70c6fe617c27071cfd15028
Owner <Owner@DESKTOP-G5P11KU>
2021-07-27 15:17:36 +0900

    test

---
+++ sample/src/sample/ALL.java
@@ -0,0 +1,34 @@
+/**
+ *
+ */
+package sample;
+
+public class ALL {
+
+	/**
+	 * @param args
+	 */
+	    public static void main(String[] args) {
+	    	int i=1;
+	      while(i<=100){
+
+	    	for( i=1;i<=100;i++){
+
+	        	 if(i%15==0){
+	        		 System.out.println("FizzBuzz");
+	        	 }else if(i%5==0){
+	        		 System.out.println("Buzz");
+	        	 }else if(i%3==0){
+	        		 System.out.println("Fizz");
+	        	 }else{
+	        		 System.out.println(i);
+	        	 }
+	        	switch(i){
+	        		 case 100:
+	        		 System.out.println("上限値になりました");
+	        		 break;
+	       }
+	    }
+	  }
+  }
+}
